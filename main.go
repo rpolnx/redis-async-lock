5fd3cda74f56b504414a428483e1774e5b99f542
Rodrigo Pereira <rodrigorpogo@gmail.com>
2024-05-24 01:00:54 -0300

    feat: removing internal go routine

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/bsm/redislock"
-	"github.com/redis/go-redis/v9"
 	publisher "github.com/rpolnx/go-redis-async-loc/internal/pub"
 	"github.com/rpolnx/go-redis-async-loc/internal/repo"
 	subscriber "github.com/rpolnx/go-redis-async-loc/internal/sub"
@@ -23,6 +22,7 @@ func main() {
 	NUM_MESSAGES, _ := strconv.Atoi(os.Getenv("NUM_MESSAGES"))
 	TOPIC_NAME := os.Getenv("TOPIC_NAME")
 	NUM_SUBSCRIBER, _ := strconv.Atoi(os.Getenv("NUM_SUBSCRIBER"))
+	KEY_NAME := os.Getenv("KEY_NAME")
 
 	redisRepo, err := repo.NewRedisRepo()
 	if err != nil {
@@ -51,76 +51,52 @@ func main() {
 
 			defer ps.Close()
 
-			// s1 := rand.NewSource(time.Now().UnixNano())
-			// r1 := rand.New(s1)
-
 			ctx := context.Background()
 			for msg := range ps.Channel() {
-				go func(msg *redis.Message) {
-					fmt.Println(fmt.Sprintf("Sub %d ->", idx+1), msg.Channel, msg.Payload)
+				fmt.Println(fmt.Sprintf("Sub %d ->", idx+1), msg.Channel, msg.Payload)
 
-					var lock *redislock.Lock
-					// defer lock.Release(ctx)
-					strategy := redislock.LimitRetry(redislock.LinearBackoff(100*time.Millisecond), 300)
-					lock, err = sub.Locker.Obtain(ctx, fmt.Sprintf("my-key-%s", TOPIC_NAME), time.Duration(30)*time.Second,
-						&redislock.Options{
-							RetryStrategy: strategy,
-						})
+				var lock *redislock.Lock
+				defer lock.Release(ctx)
+				strategy := redislock.LimitRetry(redislock.LinearBackoff(100*time.Millisecond), 300)
+				lock, err = sub.Locker.Obtain(ctx, KEY_NAME, time.Duration(30)*time.Second,
+					&redislock.Options{
+						RetryStrategy: strategy,
+					})
 
-					if err == redislock.ErrNotObtained {
-						fmt.Println("Could not obtain lock!", msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
-						return
-					} else if err != nil {
-						fmt.Printf("Unexpected lock err: %v!", err)
-					}
+				if err == redislock.ErrNotObtained {
+					fmt.Println("Could not obtain lock!", msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
+					return
+				} else if err != nil {
+					fmt.Printf("Unexpected lock err: %v!", err)
+				}
 
-					fmt.Println(fmt.Sprintf("got lock %d ->", idx+1), msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
+				fmt.Println(fmt.Sprintf("got lock %d ->", idx+1), msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
 
-					redisV, err := redisRepo.Client.Get(context.Background(), "MY_MAP").Result()
-					if err != nil {
-						log.Fatal(err)
-					}
-					personalMap := map[string]int{}
-					err = json.Unmarshal([]byte(redisV), &personalMap)
-					if err != nil {
-						log.Fatal(err)
-					}
+				redisV, err := redisRepo.Client.Get(context.Background(), "MY_MAP").Result()
+				if err != nil {
+					log.Fatal(err)
+				}
+				personalMap := map[string]int{}
+				err = json.Unmarshal([]byte(redisV), &personalMap)
+				if err != nil {
+					log.Fatal(err)
+				}
 
-					_, ok := personalMap[fmt.Sprintf("SUB_%d", idx)]
-					if !ok {
-						personalMap[fmt.Sprintf("SUB_%d", idx)] = 0
-					}
-					personalMap[fmt.Sprintf("SUB_%d", idx)] += 1
+				_, ok := personalMap[fmt.Sprintf("SUB_%d", idx)]
+				if !ok {
+					personalMap[fmt.Sprintf("SUB_%d", idx)] = 0
+				}
+				personalMap[fmt.Sprintf("SUB_%d", idx)] += 1
 
-					// keyTo := ""
-					// for k, v := range personalMap {
-					// 	//concurrent error or wrong value
-					// 	// time.Sleep(time.Duration(r1.Intn(100)) * time.Millisecond)
-					// 	if keyTo == "" {
-					// 		keyTo = k
-					// 		continue
+				b, _ = json.Marshal(personalMap)
+				r := redisRepo.Client.Set(context.Background(), "MY_MAP", b, 0)
 
-					// 	}
+				if r.Err() != nil {
+					log.Fatal(r)
+				}
 
-					// 	if personalMap[keyTo] > v {
-					// 		keyTo = k
-					// 	}
-					// }
-					// v, ok := personalMap[keyTo]
-					// if ok {
-					// 	personalMap[keyTo] += 1
-					// }
-
-					b, _ = json.Marshal(personalMap)
-					r := redisRepo.Client.Set(context.Background(), "MY_MAP", b, 0)
-
-					if r.Err() != nil {
-						log.Fatal(r)
-					}
-
-					lock.Release(ctx)
-					fmt.Println(fmt.Sprintf("release lock %d ->", idx+1), msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
-				}(msg)
+				lock.Release(ctx)
+				fmt.Println(fmt.Sprintf("release lock %d ->", idx+1), msg.Channel, msg.Payload, time.Now().Format(time.RFC3339))
 			}
 
 		}(i)
@@ -128,15 +104,12 @@ func main() {
 
 	time.Sleep(100 * time.Millisecond)
 
-	go func() {
-		for i := 0; i < NUM_MESSAGES; i++ {
-			err := pub.Publish(TOPIC_NAME, []byte(fmt.Sprintf("Message %d", i+1)))
-			if err != nil {
-				log.Fatal(err)
-			}
+	for i := 0; i < NUM_MESSAGES; i++ {
+		err := pub.Publish(TOPIC_NAME, []byte(fmt.Sprintf("Message %d", i+1)))
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
-
+	}
 	wg.Wait()
 
 	fmt.Println("ending")
